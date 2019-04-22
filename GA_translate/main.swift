1be9585861d5934b53dfce7bc472db8285e63912
geng sun <gengsun@gengdemac-mini.local>
2019-04-22 16:56:43 +0800

    Initial Commit

---
+++ GA_translate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GA_translate
+//
+//  Created by geng sun on 2019/4/22.
+//  Copyright © 2019 geng sun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
